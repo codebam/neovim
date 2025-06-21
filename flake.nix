f5413ce0c4a5d1f62411dbe1a66493df36e24050
Sean Behan <codebam@riseup.net>
2025-06-21 16:41:22 -0400

    add home manager module

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     mnw.url = "github:gerg-l/mnw";
     flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -13,69 +17,89 @@
       nixpkgs,
       mnw,
       flake-utils,
+      home-manager,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
 
-        neovim = (
-          mnw.lib.wrap pkgs {
-            neovim = pkgs.neovim-unwrapped;
-            aliases = [
-              "vi"
-              "vim"
-            ];
-            initLua = ''
-              require("config")
-            '';
-            providers = {
-              ruby.enable = false;
-              python3.enable = false;
+        # 1. The closure is now extracted into this `mnwConfig` variable.
+        # This makes it reusable for both the local package and the module.
+        mnwConfig = {
+          neovim = pkgs.neovim-unwrapped;
+          aliases = [
+            "vi"
+            "vim"
+          ];
+          initLua = ''
+            require("config")
+          '';
+          providers = {
+            ruby.enable = false;
+            python3.enable = false;
+          };
+          plugins = {
+            dev.codebam = {
+              pure = ./nvim;
             };
-            plugins = {
-              dev.codebam = {
-                pure = ./nvim;
+            start = with pkgs.vimPlugins; [
+              avante-nvim
+              blink-cmp
+              blink-cmp-copilot
+              catppuccin-vim
+              commentary
+              conform-nvim
+              copilot-lua
+              friendly-snippets
+              git-blame-nvim
+              gitsigns-nvim
+              lazydev-nvim
+              lualine-nvim
+              luasnip
+              neogit
+              nvim-autopairs
+              nvim-bqf
+              nvim-surround
+              nvim-treesitter.withAllGrammars
+              nvim-treesitter-textobjects
+              nvim-web-devicons
+              oil-nvim
+              plenary-nvim
+              sleuth
+              telescope-nvim
+              todo-comments-nvim
+              treesj
+            ];
+          };
+          extraLuaPackages = ps: [ ps.jsregexp ];
+          extraBinPath = with pkgs; [
+            bash-language-server
+            nil
+            nixd
+            git
+            markdown-oxide
+          ];
+        };
+
+        neovim = mnw.lib.wrap pkgs mnwConfig;
+
+        neovimHomeManagerModule =
+          {
+            config,
+            lib,
+            pkgs,
+            ...
+          }:
+          {
+            config = {
+              home = {
+                mnw = {
+                  enable = true;
+                } // mnwConfig;
               };
-              start = with pkgs.vimPlugins; [
-                avante-nvim
-                blink-cmp
-                blink-cmp-copilot
-                catppuccin-vim
-                commentary
-                conform-nvim
-                copilot-lua
-                friendly-snippets
-                git-blame-nvim
-                gitsigns-nvim
-                lazydev-nvim
-                lualine-nvim
-                luasnip
-                neogit
-                nvim-autopairs
-                nvim-bqf
-                nvim-surround
-                nvim-treesitter.withAllGrammars
-                nvim-treesitter-textobjects
-                nvim-web-devicons
-                oil-nvim
-                plenary-nvim
-                sleuth
-                telescope-nvim
-                todo-comments-nvim
-                treesj
-              ];
             };
-            extraLuaPackages = ps: [ ps.jsregexp ];
-            extraBinPath = with pkgs; [
-              bash-language-server
-              nil
-              nixd
-              git
-              markdown-oxide
-            ];
-          }
-        );
+          };
       in
       {
         packages.default = neovim;
@@ -90,6 +114,8 @@
           type = "app";
           program = "${neovim}/bin/nvim";
         };
+
+        homeManagerModules.default = neovimHomeManagerModule;
       }
     );
 }
