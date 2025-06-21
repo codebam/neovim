c34258438bb3efffc6cdd071b75e5883205d5306
Sean Behan <codebam@riseup.net>
2025-06-21 17:07:39 -0400

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     mnw.url = "github:gerg-l/mnw";
-    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,110 +11,146 @@
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
       mnw,
-      flake-utils,
       home-manager,
+      ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+          (
+            system:
+            function (
+              import nixpkgs {
+                inherit system;
+                config.allowUnfree = true; # As per the target example structure
+              }
+            )
+          );
 
-        # 1. The closure is now extracted into this `mnwConfig` variable.
-        # This makes it reusable for both the local package and the module.
-        mnwConfig = {
-          neovim = pkgs.neovim-unwrapped;
-          aliases = [
-            "vi"
-            "vim"
-          ];
-          initLua = ''
-            require("config")
-          '';
-          providers = {
-            ruby.enable = false;
-            python3.enable = false;
-          };
-          plugins = {
-            dev.codebam = {
-              pure = ./nvim;
+      perSystem = forAllSystems (
+        pkgs:
+        let
+          mnwConfig = {
+            neovim = pkgs.neovim-unwrapped;
+            aliases = [
+              "vi"
+              "vim"
+            ];
+            initLua = ''
+              require("config")
+            '';
+            providers = {
+              ruby.enable = false;
+              python3.enable = false;
             };
-            start = with pkgs.vimPlugins; [
-              avante-nvim
-              blink-cmp
-              blink-cmp-copilot
-              catppuccin-vim
-              commentary
-              conform-nvim
-              copilot-lua
-              friendly-snippets
-              git-blame-nvim
-              gitsigns-nvim
-              lazydev-nvim
-              lualine-nvim
-              luasnip
-              neogit
-              nvim-autopairs
-              nvim-bqf
-              nvim-surround
-              nvim-treesitter.withAllGrammars
-              nvim-treesitter-textobjects
-              nvim-web-devicons
-              oil-nvim
-              plenary-nvim
-              sleuth
-              telescope-nvim
-              todo-comments-nvim
-              treesj
+            plugins = {
+              dev.config = {
+                pure = ./nvim;
+              };
+              start = with pkgs.vimPlugins; [
+                avante-nvim
+                blink-cmp
+                blink-cmp-copilot
+                catppuccin-vim
+                commentary
+                conform-nvim
+                copilot-lua
+                friendly-snippets
+                git-blame-nvim
+                gitsigns-nvim
+                lazydev-nvim
+                lualine-nvim
+                luasnip
+                neogit
+                nvim-autopairs
+                nvim-bqf
+                nvim-surround
+                nvim-treesitter.withAllGrammars
+                nvim-treesitter-textobjects
+                nvim-web-devicons
+                oil-nvim
+                plenary-nvim
+                sleuth
+                telescope-nvim
+                todo-comments-nvim
+                treesj
+              ];
+            };
+            extraLuaPackages = ps: [ ps.jsregexp ];
+            extraBinPath = with pkgs; [
+              bash-language-server
+              nil
+              nixd
+              git
+              markdown-oxide
             ];
           };
-          extraLuaPackages = ps: [ ps.jsregexp ];
-          extraBinPath = with pkgs; [
-            bash-language-server
-            nil
-            nixd
-            git
-            markdown-oxide
-          ];
-        };
 
-        neovim = mnw.lib.wrap pkgs mnwConfig;
+          neovim = inputs.mnw.lib.wrap pkgs mnwConfig;
 
-        neovimHomeManagerModule =
-          {
-            config,
-            lib,
-            pkgs,
-            ...
-          }:
-          {
-            config = {
-              home = {
-                mnw = {
-                  enable = true;
-                } // mnwConfig;
+          neovimHomeManagerModule =
+            {
+              config,
+              lib,
+              ...
+            }:
+            {
+              config = {
+                home = {
+                  mnw = {
+                    enable = true;
+                  } // mnwConfig;
+                };
               };
             };
+        in
+        {
+          packages.default = neovim;
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              neovim
+            ];
           };
-      in
-      {
-        packages.default = neovim;
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            neovim
-          ];
-        };
+          apps.default = {
+            type = "app";
+            program = "${neovim}/bin/nvim";
+          };
 
-        apps.default = {
-          type = "app";
-          program = "${neovim}/bin/nvim";
-        };
+          homeManagerModules.default = neovimHomeManagerModule;
+        }
+      );
 
-        homeManagerModules.default = neovimHomeManagerModule;
-      }
-    );
+    in
+    {
+      packages = nixpkgs.lib.mapAttrs (_systemName: systemOutputs: systemOutputs.packages) perSystem;
+      devShells = nixpkgs.lib.mapAttrs (_systemName: systemOutputs: systemOutputs.devShells) perSystem;
+      apps = nixpkgs.lib.mapAttrs (_systemName: systemOutputs: systemOutputs.apps) perSystem;
+
+      homeManagerModules.default = (
+        { pkgs, ... }:
+        let
+          hmMnwConfig = {
+            neovim = pkgs.neovim-unwrapped;
+          };
+        in
+        {
+          config.home.mnw = {
+            enable = true;
+          } // hmMnwConfig;
+        }
+      );
+    };
 }
