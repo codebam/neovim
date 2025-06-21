8f32dbc5bc4f28ae9eb85276a612b0f5353532dc
Sean Behan <codebam@riseup.net>
2025-06-21 02:49:09 -0400

    make it runnable

--- flake.nix
+++ flake.nix
@@ -14,61 +14,65 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      neovim = (
+        mnw.lib.wrap pkgs {
+          neovim = pkgs.neovim-unwrapped;
+          aliases = [
+            "vi"
+            "vim"
+          ];
+          initLua = ''
+            																								require("codebam")
+            				'';
+          providers = {
+            ruby.enable = false;
+            python3.enable = false;
+          };
+          plugins = {
+            dev.codebam = {
+              pure = ./nvim;
+            };
+            start = with pkgs.vimPlugins; [
+              blink-cmp
+              catppuccin-vim
+              commentary
+              conform-nvim
+              friendly-snippets
+              git-blame-nvim
+              gitsigns-nvim
+              lazydev-nvim
+              lualine-nvim
+              luasnip
+              neogit
+              nvim-autopairs
+              nvim-bqf
+              nvim-surround
+              nvim-treesitter.withAllGrammars
+              nvim-treesitter-textobjects
+              nvim-web-devicons
+              oil-nvim
+              plenary-nvim
+              sleuth
+              telescope-nvim
+              todo-comments-nvim
+              treesj
+            ];
+          };
+          extraLuaPackages = ps: [ ps.jsregexp ];
+          extraBinPath = with pkgs; [
+            bash-language-server
+            nil
+            nixd
+            git
+          ];
+        }
+      );
     in
     {
+      packages.${system}.default = neovim;
       devShells.${system}.default = pkgs.mkShell {
         packages = [
-          (mnw.lib.wrap pkgs {
-            neovim = pkgs.neovim-unwrapped;
-            aliases = [
-              "vi"
-              "vim"
-            ];
-            initLua = ''
-              																								require("codebam")
-              				'';
-            providers = {
-              ruby.enable = false;
-              python3.enable = false;
-            };
-            plugins = {
-              dev.codebam = {
-                pure = ./nvim;
-              };
-              start = with pkgs.vimPlugins; [
-                blink-cmp
-                catppuccin-vim
-                commentary
-                conform-nvim
-                friendly-snippets
-                git-blame-nvim
-                gitsigns-nvim
-                lazydev-nvim
-                lualine-nvim
-                luasnip
-                neogit
-                nvim-autopairs
-                nvim-bqf
-                nvim-surround
-                nvim-treesitter.withAllGrammars
-                nvim-treesitter-textobjects
-                nvim-web-devicons
-                oil-nvim
-                plenary-nvim
-                sleuth
-                telescope-nvim
-                todo-comments-nvim
-                treesj
-              ];
-            };
-            extraLuaPackages = ps: [ ps.jsregexp ];
-            extraBinPath = with pkgs; [
-              bash-language-server
-              nil
-              nixd
-              git
-            ];
-          })
+          neovim
         ];
       };
     };
