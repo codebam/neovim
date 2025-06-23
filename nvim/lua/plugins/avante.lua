2de19f3605b21eafcf020cf476c1fe51ee7cb363
Sean Behan <codebam@riseup.net>
2025-06-23 13:38:14 -0400

    fix avante config

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -2,42 +2,49 @@ return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
 	opts = {
-		{
-			provider = "claude",
+		provider = "ollama",
+		providers = {
+			ollama = {
+				model = "devstral:latest",
+			},
+			gemini = {
+				model = "gemini-2.5-flash-preview-05-20",
+			},
+			claude = {
+				endpoint = "https://api.anthropic.com",
+				model = "claude-sonnet-4-20250514",
+				api_key_name = "cmd:pass show anthropic-api-key",
+			},
+			rag_service = {
+				__inherited_from = "ollama",
+				enabled = true,
+				host_mount = os.getenv("HOME"),
+				runner = "nix",
+				llm = {
+					provider = "ollama",
+					endpoint = "http://localhost:11434",
+					api_key = "",
+					model = "qwen3:14b",
+				},
+				embed = {
+					provider = "ollama",
+					endpoint = "http://localhost:11434",
+					api_key = "",
+					model = "nomic-embed-text:latest",
+				},
+			},
+		},
+		web_search_engine = {
+			provider = "searxng",
 			providers = {
-				ollama = {
-					model = "qwen2.5-coder:32b",
-				},
-				gemini = {
-					model = "gemini-2.5-flash-preview-05-20",
-				},
-				claude = {
-					endpoint = "https://api.anthropic.com",
-					model = "claude-sonnet-4-20250514",
-					api_key_name = { "pass", "show", "anthropic-api-key" },
-				},
-				rag_service = {
-					__inherited_from = "ollama",
-					enabled = true,
-					host_mount = os.getenv("HOME"),
-					runner = "nix",
-					llm = {
-						provider = "ollama",
-						endpoint = "http://localhost:11434",
-						api_key = "",
-						model = "qwen3:14b",
-					},
-					embed = {
-						provider = "ollama",
-						endpoint = "http://localhost:11434",
-						api_key = "",
-						model = "nomic-embed-text:latest",
-					},
+				searxng = {
+					api_url_name = "SEARXNG_API_URL",
+					query = "?q=",
 				},
 			},
-			behaviour = {
-				auto_approve_tool_permissions = true,
-			},
+		},
+		behaviour = {
+			auto_approve_tool_permissions = true,
 		},
 	},
 	dependencies = {
