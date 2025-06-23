1dbe512d9ec3d8c84d2bfea37c792db14fa4e601
Sean Behan <codebam@riseup.net>
2025-06-23 14:50:50 -0400

    enable folds

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,3 +1,10 @@
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
+
 vim.lsp.inlay_hint.enable(true)
 
 vim.lsp.enable("nixd")
