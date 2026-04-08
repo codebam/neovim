29379b073a03f87a0369c9ecd28b639213cb4114
Sean Behan <codebam@riseup.net>
2026-04-07 23:50:35 -0400

    local gemma4

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -2,45 +2,13 @@ return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
 	opts = {
-		provider = "ollama",
+		provider = "openai_compatible",
 		providers = {
-			ollama = {
-				model = "devstral:latest",
-			},
-			gemini = {
-				model = "gemini-2.5-flash-preview-05-20",
-			},
-			claude = {
-				endpoint = "https://api.anthropic.com",
-				model = "claude-sonnet-4-20250514",
-				api_key_name = "cmd:pass show anthropic-api-key",
-			},
-			rag_service = {
-				__inherited_from = "ollama",
-				enabled = true,
-				host_mount = os.getenv("HOME"),
-				runner = "nix",
-				llm = {
-					provider = "ollama",
-					endpoint = "http://localhost:11434",
-					api_key = "",
-					model = "qwen3:14b",
-				},
-				embed = {
-					provider = "ollama",
-					endpoint = "http://localhost:11434",
-					api_key = "",
-					model = "nomic-embed-text:latest",
-				},
-			},
-		},
-		web_search_engine = {
-			provider = "searxng",
-			providers = {
-				searxng = {
-					api_url_name = "SEARXNG_API_URL",
-					query = "?q=",
-				},
+			openai_compatible = {
+				__inherited_from = "openai",
+				api_key_name = "",
+				endpoint = "http://localhost:8000/v1",
+				model = "google/gemma-4-E4B-it",
 			},
 		},
 		behaviour = {
